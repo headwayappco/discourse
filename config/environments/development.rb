be1882f030ec1aaa2dfd3501341e7c80b98d590f
majkelcc <majkelcc@users.noreply.github.com>
2019-07-31 14:54:16 +0200

    chang

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 Discourse::Application.configure do
-
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
